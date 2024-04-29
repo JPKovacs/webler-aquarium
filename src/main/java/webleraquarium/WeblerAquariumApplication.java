9fdfead53ff8c3b1d2647635b6c9925a2b9b732e
Janos Kovacs <janos.kovacs@hotmail.hu>
2024-04-29 17:11:56 +0200

    Initial commit

---
+++ src/main/java/webleraquarium/WeblerAquariumApplication.java
@@ -0,0 +1,13 @@
+package webleraquarium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblerAquariumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeblerAquariumApplication.class, args);
+	}
+
+}
